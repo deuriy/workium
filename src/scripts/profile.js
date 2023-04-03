8dd3256d7933d1ed67a03475c3ffe7580cd6a827
Yuriy Demchenko <deuriy@yandex.ru>
2023-04-03 12:10:28 +0300

    Affiliate program page layout

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,6 +1,14 @@
 import $ from "jquery";
 
 $(function () {
+	$('.user-menu__link').each(function(index, link) {
+		let linkHref = $(link).attr('href');
+
+		if (window.location.pathname == linkHref) {
+			$(link).addClass('user-menu__link--active');
+		}
+	});
+
 	$('.user-sidebar__user-avatar').click(function(e) {
 		e.preventDefault();
 		$('.user-sidebar__user-info').slideToggle(200);
