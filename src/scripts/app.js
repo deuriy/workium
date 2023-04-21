9a553deeaa0bc9580d61250b91d725d2eade9831
Yuriy Demchenko <deuriy@yandex.ru>
2023-04-21 13:50:23 +0300

    Recovery page layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,26 @@
-import { Example } from "./modules/example.js";
+import $ from "jquery";
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
 
-new Example();
+$(function () {
+	Fancybox.bind("[data-fancybox]");
+	
+	Fancybox.bind(".fancybox-popup-toggle", {
+	  dragToClose: false,
+	  mainClass: 'fancybox--popup',
+	});
+
+	$('.tabs__list').each(function() {
+    $(this).find('.tabs__item').each(function(i) {
+      $(this).click(function(e) {
+        e.preventDefault();
+
+        $(this).find('.tabs-menu__link').addClass('tabs-menu__link--active');
+        $(this).siblings().find('.tabs-menu__link').removeClass('tabs-menu__link--active');
+
+        const parent = $(this).parents('.tabs');
+        parent.find('.tabs__content').hide();
+        parent.find('.tabs__content:eq(' + i + ')').show();
+      });
+    });
+  });
+});
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,28 +3,6 @@ import Swiper, { Navigation } from 'swiper';
 import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
 
 $(function () {
-  Fancybox.bind("[data-fancybox]");
-
-  Fancybox.bind(".fancybox-popup-toggle", {
-    dragToClose: false,
-    mainClass: 'fancybox--popup',
-  });
-
-  $('.tabs__list').each(function() {
-    $(this).find('.tabs__item').each(function(i) {
-      $(this).click(function(e) {
-        e.preventDefault();
-
-        $(this).find('.tabs-menu__link').addClass('tabs-menu__link--active');
-        $(this).siblings().find('.tabs-menu__link').removeClass('tabs-menu__link--active');
-
-        const parent = $(this).parents('.tabs');
-        parent.find('.tabs__content').hide();
-        parent.find('.tabs__content:eq(' + i + ')').show();
-      });
-    });
-  });
-
   const peopleSwiper = new Swiper('.people-swiper', {
     modules: [Navigation],
     loop: true,
