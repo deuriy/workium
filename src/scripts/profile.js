3a61e168b0db381a00583a512c06f257c72edb75
Yuriy Demchenko <deuriy@yandex.ru>
2023-05-26 23:26:34 +0300

    Change user info block

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -11,7 +11,7 @@ $(function () {
 
 	$('.user-sidebar__user-avatar').click(function(e) {
 		e.preventDefault();
-		$('.user-sidebar__user-info').slideToggle(200);
+		$('.user-sidebar__user-info-wrapper').slideToggle(200);
 	});
 
 	$('.mobile-header__menu-hamburger').click(function(e) {
