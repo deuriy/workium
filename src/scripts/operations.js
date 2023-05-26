d1249a5604daaa90a05201cf7bc8c603f3457fb4
Yuriy Demchenko <deuriy@yandex.ru>
2023-05-26 14:46:41 +0300

    Mobile version of opertions page

---
+++ src/scripts/operations.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+
+document.addEventListener('DOMContentLoaded', function () {
+	const walletsSwiper = new Swiper('.wallets-swiper', {
+    // loop: true,
+    slidesPerView: 'auto',
+    // centeredSlides: true,
+    spaceBetween: 20,
+  });
+});
