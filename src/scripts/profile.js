45a5cc39e52e97140da8dad596403e229e0479c7
Yuriy Demchenko <deuriy@yandex.ru>
2023-06-03 16:51:22 +0300

    Adding context menu to user sidebar, change styles of user info block

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,5 +1,12 @@
 import $ from "jquery";
 
+function copyLink(input) {
+	input.select();
+  input.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+}
+
 $(function () {
 	$('.user-menu__link').each(function(index, link) {
 		let linkHref = $(link).attr('href');
@@ -9,10 +16,10 @@ $(function () {
 		}
 	});
 
-	$('.user-sidebar__user-avatar').click(function(e) {
-		e.preventDefault();
-		$('.user-sidebar__user-info-wrapper').slideToggle(200);
-	});
+	// $('.user-sidebar__user-avatar').click(function(e) {
+	// 	e.preventDefault();
+	// 	$('.user-sidebar__user-info-wrapper').slideToggle(200);
+	// });
 
 	$('.mobile-header__menu-hamburger').click(function(e) {
 		$(this).toggleClass('menu-hamburger--opened');
@@ -25,10 +32,7 @@ $(function () {
 
 		if (!$copyLinkInput.length) return;
 
-		$copyLinkInput[0].select();
-	  $copyLinkInput[0].setSelectionRange(0, 99999);
-
-	  document.execCommand("copy");
+		copyLink($copyLinkInput[0]);
 	});
 
 	$('[data-bookmark]').click(function(e) {
@@ -42,4 +46,59 @@ $(function () {
 	}, function() {
 		$(this).find('.more-link').removeClass('more-link--hover more-link--full-width-hover');
 	});
+
+
+	$('[data-context-block-trigger]').click(function(e) {
+    let $contextBlock = $($(this).attr('href'));
+    $contextBlock.toggleClass('context-block--opened');
+
+    e.preventDefault();
+  });
+
+  $(document).click(function(e) {
+    if (!$(e.target).closest('.context-block').length && !$(e.target).closest('[data-context-block-trigger]').length) {
+      $('.context-block--opened').removeClass('context-block--opened');
+    }
+  });
+
+  $('.context-menu__link--copy-link').click(function(e) {
+  	let $copyLinkInput = $($(this).attr('href'));
+
+  	copyLink($copyLinkInput[0]);
+
+  	$(this).find('.context-menu__link-title').text('Посилання скопійовано!');
+  	$(this).find('.context-menu__icon').attr('src', '/img/context_menu/checked_ok.svg');
+
+  	setTimeout(() => {
+  		$(this).closest('.context-block').removeClass('context-block--opened');
+
+  		$(this).find('.context-menu__link-title').text('Партнерське посилання');
+  		$(this).find('.context-menu__icon').attr('src', '/img/context_menu/link.svg');
+  	}, 1500);
+
+    e.preventDefault();
+  });
+
+  $('.context-menu__link--hide-profile').click(function(e) {
+  	let $userAvatarLink = $('.user-sidebar__user-avatar-link');
+  	let $userInfoWrapper = $('.user-sidebar__user-info-wrapper');
+
+  	$userAvatarLink.toggleClass('user-sidebar__user-avatar-link--profile-shown');
+
+  	if (!$userAvatarLink.hasClass('user-sidebar__user-avatar-link--profile-shown')) {
+	  	$(this).find('.context-menu__link-title').text('Показати профіль');
+	  	$(this).find('.context-menu__icon').attr('src', '/img/context_menu/show_profile.svg');
+	  	// $userInfoWrapper.slideUp();
+	  	$userInfoWrapper.hide();
+  	} else {
+  		$(this).find('.context-menu__link-title').text('Приховати профіль');
+	  	$(this).find('.context-menu__icon').attr('src', '/img/context_menu/hide_profile.svg');
+	  	// $userInfoWrapper.slideDown();
+	  	$userInfoWrapper.show();
+  	}
+
+  	$(this).closest('.context-block').removeClass('context-block--opened');
+
+  	e.preventDefault();
+  });
 });
