08055fdd12f1f9fef352eedb7fa5e9ab57e0543e
Yuriy Demchenko <deuriy@yandex.ru>
2023-06-10 15:48:13 +0300

    Mobile version of withdrawal funds page

--- src/scripts/operations.js
+++ src/scripts/operations.js
@@ -1,4 +1,6 @@
+import $ from "jquery";
 import Swiper from 'swiper';
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
 
 document.addEventListener('DOMContentLoaded', function () {
 	const walletsSwiper = new Swiper('.wallets-swiper', {
@@ -6,5 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     slidesPerView: 'auto',
     // centeredSlides: true,
     spaceBetween: 23,
+  });
+
+  Fancybox.bind(".payment-systems-popup-link", {
+    dragToClose: false,
+    mainClass: 'fancybox--payment-systems-popup',
+  });
+
+  $('.bank__input').change(function(event) {
+    // let id = $(this).val();
+    let $bank = $(this).closest('.bank');
+
+    $('.payment-systems-popup-link__name').text($bank.find('.bank__name').text());
+    $('.payment-systems-popup-link__currency').text($bank.find('.bank__currency').text());
+    $('.payment-systems-popup-link__icon').attr('src', $bank.find('.bank__icon').attr('src'));
+
+    // console.log($(this).val());
+    Fancybox.close();
   });
 });
