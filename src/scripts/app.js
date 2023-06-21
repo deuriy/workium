4cea8560ab687b6312b9e04ead283cf13086e8d5
Yuriy Demchenko <deuriy@yandex.ru>
2023-06-21 22:17:43 +0300

    Editing confetti script

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,6 +13,31 @@ function fire(particleRatio, opts) {
   }));
 }
 
+function runConfetti() {
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
+
 $(function () {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -57,64 +82,11 @@ $(function () {
         }
 
         currentFancybox = fancybox;
-
-        // console.log(slide.src);
-        // if (slide.src.includes('award-popup')) {
-        //   console.log('confetti!');
-
-        //   fire(0.25, {
-        //     spread: 26,
-        //     startVelocity: 55,
-        //   });
-        //   fire(0.2, {
-        //     spread: 60,
-        //   });
-        //   fire(0.35, {
-        //     spread: 100,
-        //     decay: 0.91,
-        //     scalar: 0.8
-        //   });
-        //   fire(0.1, {
-        //     spread: 120,
-        //     startVelocity: 25,
-        //     decay: 0.92,
-        //     scalar: 1.2
-        //   });
-        //   fire(0.1, {
-        //     spread: 120,
-        //     startVelocity: 45,
-        //   });
-        // }
       },
 
       done: (fancybox, slide) => {
-        console.log('done!');
-
         if (slide.src.includes('award-popup')) {
-          console.log('confetti!');
-
-          fire(0.25, {
-            spread: 26,
-            startVelocity: 55,
-          });
-          fire(0.2, {
-            spread: 60,
-          });
-          fire(0.35, {
-            spread: 100,
-            decay: 0.91,
-            scalar: 0.8
-          });
-          fire(0.1, {
-            spread: 120,
-            startVelocity: 25,
-            decay: 0.92,
-            scalar: 1.2
-          });
-          fire(0.1, {
-            spread: 120,
-            startVelocity: 45,
-          });
+          runConfetti();
         }
       }
     }
