e02ee01b0b618a0b942e302810dba41db13c08bc
Yuriy Demchenko <deuriy@yandex.ru>
2023-06-21 12:13:46 +0300

    Vacancies filter layout

--- src/scripts/vacancies.js
+++ src/scripts/vacancies.js
@@ -1,17 +1,61 @@
 import Swiper, { Pagination } from 'swiper';
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/fancybox.esm.js";
 
 document.addEventListener('DOMContentLoaded', function () {
-	const promoBlocksSwiper = new Swiper('.promo-blocks-swiper', {
-    modules: [Pagination],
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 8,
+  Fancybox.bind(".additional-filters-popup-link", {
+    dragToClose: false,
+    mainClass: 'fancybox--additional-filters-popup',
 
-    pagination: {
-	    el: '.promo-blocks-swiper__pagination',
-	    bulletActiveClass: 'swiper-pagination-bullet--active',
-	    // clickable: true
-	  },
+    tpl: {
+      closeButton: '<button data-fancybox-close class="fancybox-close-button hidden-xxs" title="{{CLOSE}}"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 18 18"><path stroke="#A1A7B3" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.4" d="M1 17 17 1M1 1l16 16"></path></svg></button>'
+    }
   });
+
+  document.addEventListener('click', function (e) {
+    let filterTag = e.target.closest('.filter-tag');
+
+    if (!filterTag) return;
+
+    filterTag.classList.toggle('filter-tag--selected');
+  });
+
+  document.addEventListener('click', function (e) {
+    let checkboxesGroupTitle = e.target.closest('.checkboxes-group__title');
+
+    if (!checkboxesGroupTitle) return;
+
+    let checkboxesGroup = checkboxesGroupTitle.closest('.checkboxes-group');
+
+    if (!checkboxesGroup) return;
+
+    checkboxesGroup.classList.toggle('checkboxes-group--closed');
+  });
+
+  document.addEventListener('click', function (e) {
+    let radioBtnsGroupTitle = e.target.closest('.radiobtns-group__title');
+
+    if (!radioBtnsGroupTitle) return;
+
+    let radioBtnsGroup = radioBtnsGroupTitle.closest('.radiobtns-group');
+
+    if (!radioBtnsGroup) return;
+
+    radioBtnsGroup.classList.toggle('radiobtns-group--closed');
+  });
+
+  if (document.documentElement.clientWidth < 768) {
+  	const promoBlocksSwiper = new Swiper('.promo-blocks-swiper', {
+      modules: [Pagination],
+      loop: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 8,
+
+      pagination: {
+  	    el: '.promo-blocks-swiper__pagination',
+  	    bulletActiveClass: 'swiper-pagination-bullet--active',
+  	    // clickable: true
+  	  },
+    });
+  }
 });
