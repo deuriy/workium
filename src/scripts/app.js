ad3effdc3de2065c69c2edde854089e640b3deec
Yuriy Demchenko <deuriy@yandex.ru>
2023-07-10 03:26:33 +0300

    Changing the user menu and menu on the main page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -219,8 +219,17 @@ $(function () {
 
   // Show/hide dropdown block
   $('[data-dropdown-block-trigger]').click(function(e) {
+    let $currentVisibleDropdownBlock = $('.dropdown-block--visible');
+    // console.log($currentVisibleDropdownBlock);
+    
     let $dropdownBlock = $($(this).attr('href'));
-    $dropdownBlock.toggleClass('dropdown-block--visible');
+
+    if ($currentVisibleDropdownBlock != $dropdownBlock) {
+      $currentVisibleDropdownBlock.removeClass('dropdown-block--visible');
+      // console.log('No!');
+    }
+    
+    $dropdownBlock.addClass('dropdown-block--visible');
 
     e.preventDefault();
   });
