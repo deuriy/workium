5a9698c71e4fa3fdb5b2d92468023c3d50a47a85
Yuriy Demchenko <deuriy@yandex.ru>
2023-09-15 15:27:07 +0300

    Adding a clear button to the city search field

--- src/scripts/vacancies.js
+++ src/scripts/vacancies.js
@@ -33,6 +33,19 @@ function checkFilterFill() {
   isFilterChanged() ? $clearBtn.show() : $clearBtn.hide();
 }
 
+function clearCitiesSelect () {
+  let $citiesSelectToggles = $('.filter__cities-select-toggle, .additional-filters__cities-select-toggle');
+  $citiesSelectToggles.each(function(index, el) {
+    let citiesSelectToggleText = $(el).data('default-placeholder');
+
+    $('#cities-select').multiSelect('deselect_all');
+    $('#additional-filter-cities-select').multiSelect('deselect_all');
+
+    $(el).html(citiesSelectToggleText);
+    $(el).removeClass('select-toggle--selected');
+  });
+}
+
 function removeItemFromArray (array, value) {
   let index = array.indexOf(value);
 
@@ -43,44 +56,56 @@ function removeItemFromArray (array, value) {
   return array;
 }
 
-// const compareArrays = (a, b) => {
-//   return JSON.stringify(a) === JSON.stringify(b);
-// };
-
 $(() => {
   $(".cities-select").each((index, el) => {
     $(el).multiSelect({
-      selectableHeader: '<div class="ms-selectable__header"><div class="ms-selectable__search-input-box"><input type="search" name="ms_search" class="ms-selectable__search-input" placeholder="Введіть назву міста…" title="Введіть назву міста…" /></div></div>',
+      selectableHeader: '<div class="ms-selectable__header"><div class="ms-selectable__search-input-box"><input type="search" name="ms_search" class="ms-selectable__search-input" placeholder="Введіть назву міста…" title="Введіть назву міста…" /><button type="button" class="ms-selectable__clear-search-btn" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18"><path stroke="#A1A7B3" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.4" d="M1 17 17 1M1 1l16 16"></path></svg></button></div></div>',
       selectionHeader: '<div class="ms-selection__header"><div class="ms-selection__label">Обрані міста</div><a href="#" class="btn-beige btn-beige--filter ms-selection__clear-btn">Очистити</a></div>',
       keepOrder: true,
       cssClass: 'ms-container--default',
 
       afterInit: function (ms) {
         let that = this;
+        let $searchInput = that.$container.find('.ms-selectable__search-input');
+        let $clearSearchBtn = that.$container.find('.ms-selectable__clear-search-btn');
+        let $selectableItems = that.$selectableUl.children().toArray();
+        let psArr = [];
 
-        this.$selectionContainer.append('<div class="ms-selection__no-results">У вас ще немає обраних міст...</div>');
-        this.$container.append('<div class="ms-container__footer"><button type="button" class="btn-grey btn-grey--multi-select ms-container__apply-btn">Застосувати</button></div>');
+        that.$selectionContainer.append('<div class="ms-selection__no-results">У вас ще немає обраних міст...</div>');
+        that.$container.append('<div class="ms-container__footer"><button type="button" class="btn-grey btn-grey--multi-select ms-container__apply-btn">Застосувати</button></div>');
 
-        $([this.$selectableUl[0], this.$selectionUl[0]]).each(function(index, item) {
-          new PerfectScrollbar(item, {
+        $([that.$selectableUl[0], that.$selectionUl[0]]).each(function(index, item) {
+          psArr.push(new PerfectScrollbar(item, {
             wheelSpeed: 2,
             wheelPropagation: false,
             minScrollbarLength: 20,
             suppressScrollX: true
-          });
+          }));
         });
 
-        this.$container.find('.ms-selectable__search-input').on('input', function(event) {
-          let value = $(this).val().toLowerCase().trim();
-          let $selectableItemsLabels = that.$selectableUl.children().toArray();
+        console.log(this.$selectableUl);
+        console.log(psArr);
 
-          $selectableItemsLabels.forEach(item => {
+        $searchInput.on('input', function(event) {
+          let value = $(this).val().toLowerCase().trim();
+
+          $selectableItems.forEach(item => {
             if (!$(item).text().toLowerCase().trim().includes(value)) {
               $(item).hide();
             } else {
               $(item).show();
             }
           });
+
+          value ? $clearSearchBtn.show() : $clearSearchBtn.hide();
+
+          psArr[0].update();
+        });
+
+        $clearSearchBtn.click(function(event) {
+          $(this).hide();
+
+          $searchInput.val('').trigger('input').focus();
         });
 
         $('.ms-selection__clear-btn').click(function(event) {
@@ -152,19 +177,6 @@ $(() => {
       }
     });
   });
-
-  function clearCitiesSelect () {
-    let $citiesSelectToggles = $('.filter__cities-select-toggle, .additional-filters__cities-select-toggle');
-    $citiesSelectToggles.each(function(index, el) {
-      let citiesSelectToggleText = $(el).data('default-placeholder');
-
-      $('#cities-select').multiSelect('deselect_all');
-      $('#additional-filter-cities-select').multiSelect('deselect_all');
-
-      $(el).html(citiesSelectToggleText);
-      $(el).removeClass('select-toggle--selected');
-    });
-  }
 
   $('.filter__clear-btn, .additional-filters__clear-btn, .additional-filters__clear-filter-btn').click(clearCitiesSelect);
 
