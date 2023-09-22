810befc69f94fb1d4a0b5f09aa2aa23f0d4b11b6
Yuriy Demchenko <deuriy@yandex.ru>
2023-09-22 14:23:02 +0300

    Job copy button

--- src/scripts/vacancy.js
+++ src/scripts/vacancy.js
@@ -1,6 +1,28 @@
 import $ from "jquery";
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+function copyText(input) {
+  input.select();
+  input.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+}
+
+function copyVacancyText () {
+  let $vacancyTextWrapper = $('.vacancy-card__text-wrapper');
+  let $vacancyTitle = $vacancyTextWrapper.find('.vacancy-card__title').text().trim();
+  let $vacancyText = $vacancyTextWrapper.find('.vacancy-card__text').text().trim();
+
+  if (!$vacancyTextWrapper.length) return;
+
+  $vacancyTextWrapper.after(`<textarea class="vacancy-card__textarea">${$vacancyTitle + '\r\n' + $vacancyText}</textarea>`);
+
+  let $vacancyCardTextarea = $('.vacancy-card__textarea');
+
+  copyText($vacancyCardTextarea[0]);
+  $vacancyCardTextarea.remove();
+}
+
 function toggleMoreLink ($link) {
   let linkText = $link.text() === 'Приховати' ? 'Детальніше' : 'Приховати';
   $link.text(linkText);
@@ -41,19 +63,21 @@ $(() => {
     }
   });
 
-  const promoBlocksSwiper = new Swiper('.promo-blocks-swiper', {
-    modules: [Pagination],
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 8,
+  if ($(window).width() < 768) {
+    const promoBlocksSwiper = new Swiper('.promo-blocks-swiper', {
+      modules: [Pagination],
+      loop: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 8,
 
-    pagination: {
-      el: '.promo-blocks-swiper__pagination',
-      bulletActiveClass: 'swiper-pagination-bullet--active',
-      // clickable: true
-    },
-  });
+      pagination: {
+        el: '.promo-blocks-swiper__pagination',
+        bulletActiveClass: 'swiper-pagination-bullet--active',
+        // clickable: true
+      },
+    });
+  }
 
   $('.infoblock__more-link').click(function(e) {
     $(this).closest('.infoblock').find('.infoblock__text').toggleClass('infoblock__text--truncated');
@@ -74,6 +98,34 @@ $(() => {
 
   $('.vacancy-footer__bookmark-btn').click(function(event) {
     $('.vacancy-info__bookmark-icon').toggleClass('bookmark-icon--fill');
+  });
+
+  $('.vacancy-card__copy-btn').click(function(event) {
+    copyVacancyText();
+
+    let defaultText = $(this).text();
+
+    $(this).addClass('btn-white--copied');
+    $(this).text('Текст скопійовано!');
+
+    setTimeout(() => {
+      $(this).removeClass('btn-white--copied');
+      $(this).text(defaultText);
+    }, 2000);
+
+    event.preventDefault();
+  });
+
+  $('.vacancy-footer__copy-btn').click(function(event) {
+    copyVacancyText();
+
+    $(this).addClass('btn-grey--copied');
+
+    setTimeout(() => {
+      $(this).removeClass('btn-grey--copied');
+    }, 2000);
+
+    event.preventDefault();
   });
 
   // $('.fancybox-popup__show-contacts-btn').click(function(e) {
