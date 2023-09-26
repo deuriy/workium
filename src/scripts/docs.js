b639248491fdab9c66f02b3138c00be610d8be12
Yuriy Demchenko <deuriy@yandex.ru>
2023-09-26 23:36:30 +0300

    Change article box styles

--- src/scripts/docs.js
+++ src/scripts/docs.js
@@ -1,6 +1,33 @@
 import $ from "jquery";
+import Swiper, { Navigation } from 'swiper';
 
 $(() => {
+	const peopleSwiper = new Swiper('.people-swiper', {
+    modules: [Navigation],
+    loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 8,
+
+    navigation: {
+      nextEl: '.people-swiper-container__next',
+      prevEl: '.people-swiper-container__prev',
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 16,
+        centeredSlides: false
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+        centeredSlides: false,
+      }
+    }
+  });
+
 	$('.article-box__more-btn').click(function(event) {
 		let $articleBoxContent = $(this).closest('.article-box').find('.article-box__content');
 
