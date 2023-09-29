8547554201f0593e82485e8cd7be7ddadafe40fa
Yuriy Demchenko <deuriy@yandex.ru>
2023-09-29 10:05:01 +0300

    Changing testimonials slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -29,15 +29,28 @@ $(() => {
   });
 
   const testimonialsSwiper = new Swiper('.testimonials-swiper', {
+    modules: [Navigation],
     loop: true,
     slidesPerView: 'auto',
     centeredSlides: true,
     spaceBetween: 8,
 
+    navigation: {
+      nextEl: '.testimonials-swiper-container__next',
+      prevEl: '.testimonials-swiper-container__prev',
+    },
+
     breakpoints: {
       768: {
-        loop: false
+        slidesPerView: 3,
+        spaceBetween: 16,
+        centeredSlides: false,
       },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+        centeredSlides: false,
+      }
     }
   });
 
