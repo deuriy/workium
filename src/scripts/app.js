6f3ef216aa742573a7f7284e20c824640017202c
Yuriy Demchenko <deuriy@yandex.ru>
2023-09-29 11:21:39 +0300

    Changing award popup

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -140,7 +140,7 @@ $(() => {
       done: (fancybox, slide) => {
         // console.log(slide);
 
-        if (!slide.contentEl.classList.contains('fancybox-popup--award-dark-bg')) {
+        if (slide.src.includes('award-') && !slide.contentEl.classList.contains('fancybox-popup--award-dark-bg')) {
           runConfetti();
         } else if (slide.src.includes('employment-smart-work-popup')) {
           // let $showContactsBtn = $(slide.contentEl).find('.fancybox-popup__show-contacts-btn');
