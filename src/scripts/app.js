8242151811f3ba7c85b4accce61d05fd7019796f
Yuriy Demchenko <yu.demche@gmail.com>
2025-04-09 04:34:26 +0300

    Changing vacancies pages

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -664,6 +664,16 @@ $(() => {
   $(document).on('mouseover', '[data-tooltip]', function(event) {
     let $tooltip = $(this).find('.tooltip');
     $tooltip.addClass('tooltip--visible');
+
+    let windowHeight = document.documentElement.clientHeight;
+    let tooltipTriggerCoords = this.getBoundingClientRect();
+    let tooltipHeight = $tooltip.outerHeight();
+
+    if (windowHeight < tooltipTriggerCoords.bottom + tooltipHeight + 10) {
+      $tooltip.addClass('tooltip--extended-top').removeClass('tooltip--extended-bottom');
+    } else {
+      $tooltip.removeClass('tooltip--extended-top').addClass('tooltip--extended-bottom');
+    }
   });
 
   $(document).on('mouseout', '[data-tooltip]', function(event) {
